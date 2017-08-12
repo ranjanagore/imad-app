e4a0b23b92e73bc43850f5e2adc6ea507b47b31d
ranjanagore <30624026+ranjanagore@users.noreply.github.com>
2017-08-12 17:42:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,12 +6,10 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'temp.html'));
 });
+
+
 
 app.get('/article-one', function(req ,res){
 
